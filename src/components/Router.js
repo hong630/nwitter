ffc2bf6524b19802b8764371c4043d9911a10704
hongin-jo <85662073+hongin-jo@users.noreply.github.com>
2021-11-03 21:20:34 +0900

    just saving

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,2 +1,17 @@
-import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import React, {useState} from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+export default ()=>{
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return(
+      <Router>
+          <Switch>
+              {isLoggedIn?
+              <>
+              <Route>
+                  </Route>
+              </>: }
+          </Switch>
+      </Router>
+  )
+}
